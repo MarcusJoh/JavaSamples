63eae14510b305d9f64d592e15077b9b44184714
marcus johansson <mj.emeth@gmail.com>
2020-02-03 13:45:31 +0100

    Starting sample for wed services

---
+++ Java3/demo-1/src/main/java/com/example/demo/Helloworls.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+//Controller  
+@RestController
+public class Helloworls {
+
+//using get method and hello-world as URI  
+	@RequestMapping(method = RequestMethod.GET, path = "/hello-world")
+	public String helloWorld() {
+		return "Hello World";
+	}
+
+	@RequestMapping(value = "/score", method = RequestMethod.GET)
+	public String update(String rev) {
+		StringBuilder sb = new StringBuilder(rev);
+
+		return sb.reverse().toString();
+	}
+
+}
