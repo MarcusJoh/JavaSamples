3ecf70268327f4d999972627dc4c79d746a89efb
marcus johansson <mj.emeth@gmail.com>
2020-12-16 23:16:50 +0100

    Data

---
+++ Java5/BaseSQL/src/DatabaseLogin.java
@@ -0,0 +1,25 @@
+
+public class DatabaseLogin {
+
+	
+	private static String uName="root";
+	private static String uPass="";
+
+	public static String getuName() {
+		return uName;
+	}
+
+	public void setuName(String uName) {
+		DatabaseLogin.uName = uName;
+	}
+
+	public static String getuPass() {
+		return uPass;
+	}
+
+	public void setuPass(String uPass) {
+		DatabaseLogin.uPass = uPass;
+	}
+	
+	
+}
